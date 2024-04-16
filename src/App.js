d7cd7ad33302adb5dfb191171c9762785cf7e22f
Rashed <ahmedha8061@gmail.com>
2024-04-17 00:24:13 +0200

    Final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Questions from "./Questions";
 
-function App() {
+const initialState = {
+  questions: {},
+  //stateus:  active , loading ,error , active
+  status: "Loading",
+  index: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "recivedData":
+      return { ...state, questions: action.payload, status: action.type };
+    case "dataFaild":
+      return { ...state, status: action.type };
+    case "active":
+      return { ...state, status: action.type };
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
+export default function App() {
+  const [{ status, questions, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+  const NumOfQuestions = questions.legnth;
+
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "recivedData", payload: data }))
+      .catch((err) => dispatch({ type: "dataFaild" }));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "Loading" && <Loader />}
+        {status === "dataFaild" && <Error />}
+        {status === "recivedData" && (
+          <StartScreen NumOfQuestions={NumOfQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Questions questions={questions[index]} />}
+      </Main>
     </div>
   );
 }
-
-export default App;
